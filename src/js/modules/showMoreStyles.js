2cc6cb712fdaa633f85a068cb5d9174a0d2f7a71
Mi4manMac <turovskydo@gmail.com>
2022-04-06 10:16:38 +0300

    little changes with downloading cards

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -21,31 +21,28 @@ const showMoreStyles = (trigger, wrapper) => {
 
     //     });
     // });
-    
-
-    btn.addEventListener('click', function() {
+    btn.addEventListener('click', (e) => {
         getResource('http://localhost:3000/styles')
             .then(res => creatCards(res))
             .catch(error => console.log(error));
-        this.remove();
+        e.target.remove();
     });
 
     function creatCards(response) {
-        response.forEach(({ src, title, link}) => {
-            let card = document.createElement('div');
+        response.forEach(({src, title, link}) => {
+            const card = document.createElement('div');
             card.classList.add('animate__animated', 'animate__fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
-
             card.innerHTML = `
             <div class=styles-block>
-                <img src=${src} alt="style">
-                <h4>${title}</h4>
-                <a href=${link}>Подробнее</a>
+                 <img src=${src} alt="style">
+                 <h4>${title}</h4>
+                 <a href=${link}>Подробнее</a>
             </div>
             `;
-
-            document.querySelector(wrapper).appendChild(card);
+            document.querySelector(wrapper).append(card);
         });
     }
+
 
 };
 
