e996aba5b576965d63e09188a0aaa3705144c451
Dmyto_Turovskiy <turovskydo@gmail.com>
2022-04-10 21:25:29 +0300

    small changes with forms

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,66 +1,177 @@
-import { postData } from "../services/requests";
+// import { postData } from "../services/requests";
 
 
-const forms = (state) => {
+// const forms = (state) => {
 
 
-    const forms = document.querySelectorAll('form'),
-        inputs = document.querySelectorAll('input'),
-        upload = document.querySelectorAll('[name="upload"]');
+//     const forms = document.querySelectorAll('form'),
+//         inputs = document.querySelectorAll('input'),
+//         upload = document.querySelectorAll('[name="upload"]');
+    
+//     const message = {
+//         loading: 'Загрузка...',
+//         success: "Спасибо. Мы скоро с вами свяжемя",
+//         failure: 'Что то пошло не так',
+//         spinner: 'assets/img/spinner.gif',
+//         ok: 'assets/img/ok.png',
+//         fail: 'assets/img/fail.png'
+//     };
+
+//     const path = {
+//         designer: 'http://localhost:3000/request',
+//         question: 'http://localhost:3000/questions'
+//     };
+
+
+//     const clearInputs = ()=>{
+//         inputs.forEach(item=>{
+//             item.value = '';
+//         });
+//         upload.forEach(item=>{
+//             item.previousElementSibling.textContent = 'Файл не выбран';
+//         });
+//     };
+
+
+//     upload.forEach(item=>{
+//         item.addEventListener('input', ()=>{
+//             console.log(item.files[0]); // тут ми звертаємось до інпута і у нього тепер є властивість files
+//             let dots;
+//             const arr = item.files[0].name.split('.');
+//             // 'dsfdfdskfsd.jpg' => ['dsfdfdskfsd', 'jpg']
+//             arr[0].length > 6 ? dots = '...' : dots = '.';
+//             const name = arr[0].substring(0, 6) + dots + arr[1];
+//             item.previousElementSibling.textContent = name;
+//         });
+//     });
+
+//     forms.forEach((item, i) => {
+//         item.addEventListener('submit', (e) => {
+//             e.preventDefault();
+
+//             let statusMessage = document.createElement('div');
+//             statusMessage.classList.add('status');
+//             // statusMessage.style.textAlign = 'center';
+//             item.parentNode.appendChild(statusMessage);
+//             item.classList.add('animate__animated', 'animate__fadeOutUp');
+//             setTimeout(()=>{
+//                 item.style.display = 'none';
+//             }, 400);
+
+//             let statusImg = document.createElement('img');
+//             statusImg.setAttribute('src', message.spinner);
+//             statusImg.classList.add('animate__animated', 'animate__fadeInUp');
+//             statusMessage.appendChild(statusImg);
+
+//             let textMessage = document.createElement('div');
+//             textMessage.textContent = message.loading;
+//             statusMessage.appendChild(textMessage);
+
+//             const formData = new FormData(item);
+//             if (item.classList.contains('calc_form')) {
+//                 for (let key in state) {
+//                     formData.append(key, state[key]);
+//                 }
+//             }
+//             console.log(formData);
+//             let api;
+//             item.closest('.popup-design') || item.classList.contains('calc_form') ? api = path.designer : api = path.question;
+//             console.log(api);
+//             const json = JSON.stringify(Object.fromEntries(formData.entries()));
+//             postData(api, json)
+//                 .then(res => {
+//                     console.log(res);
+//                     statusImg.setAttribute('src', message.ok);
+//                     textMessage.textContent = message.success;
+//                 })
+//                 .catch(() => {
+//                     statusImg.setAttribute('src', message.fail);
+//                     textMessage.textContent = message.failure;
+//                 })
+//                 .finally(() => {
+//                     if (item.classList.contains('calc_form')) {
+//                         document.querySelector('#size').value = '';
+//                         document.querySelector('#material').value = '';
+//                         document.querySelector('#options').value = '0';
+//                         document.querySelector('.promocode').value = '';
+//                     }
+//                     clearInputs();
+
+//                     setTimeout(() => {
+//                         statusMessage.remove();
+//                         item.style.display = 'block';
+//                         item.classList.remove('animate__fadeOutUp');
+//                         item.classList.add('animate__fadeInUp');
+//                     }, 5000);
+//                 });
+
+//         });
+        
+//     });
+
+
+// };
+
+
+// export default forms;
+import {postData} from '../services/requests';
+
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+          inputs = document.querySelectorAll('input'),
+          upload = document.querySelectorAll('[name="upload"]');
     
     const message = {
         loading: 'Загрузка...',
-        success: "Спасибо. Мы скоро с вами свяжемя",
-        failure: 'Что то пошло не так',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...',
         spinner: 'assets/img/spinner.gif',
         ok: 'assets/img/ok.png',
-        fail: 'assets/img/fail.png'      
+        fail: 'assets/img/fail.png'
     };
 
     const path = {
-        designer: 'http://localhost:3000/request',
-        question: 'http://localhost:3000/questions'
+        designer: 'assets/server.php',
+        question: 'assets/question.php'
     };
 
-
-    const clearInputs = ()=>{
-        inputs.forEach(item=>{
+    const clearInputs = () => {
+        inputs.forEach(item => {
             item.value = '';
         });
-        upload.forEach(item=>{
-            item.previousElementSibling.textContent = 'Файл не выбран';
+        upload.forEach(item => {
+            item.previousElementSibling.textContent = "Файл не выбран";
         });
     };
 
-
-    upload.forEach(item=>{
-        item.addEventListener('input', ()=>{
-            console.log(item.files[0]); // тут ми звертаємось до інпута і у нього тепер є властивість files
+    upload.forEach(item => {
+        item.addEventListener('input', () => {
+            console.log(item.files[0]);
             let dots;
             const arr = item.files[0].name.split('.');
-            // 'dsfdfdskfsd.jpg' => ['dsfdfdskfsd', 'jpg']
-            arr[0].length > 6 ? dots = '...' : dots = '.';
+
+            arr[0].length > 6 ? dots = "..." : dots = '.';
             const name = arr[0].substring(0, 6) + dots + arr[1];
             item.previousElementSibling.textContent = name;
         });
     });
 
-    forms.forEach((item, i) => {
+    form.forEach(item => {
         item.addEventListener('submit', (e) => {
             e.preventDefault();
 
             let statusMessage = document.createElement('div');
             statusMessage.classList.add('status');
-            // statusMessage.style.textAlign = 'center';
             item.parentNode.appendChild(statusMessage);
-            item.classList.add('animate__animated', 'animate__fadeOutUp');
-            setTimeout(()=>{
+
+            item.classList.add('animated', 'fadeOutUp');
+            setTimeout(() => {
                 item.style.display = 'none';
             }, 400);
 
             let statusImg = document.createElement('img');
             statusImg.setAttribute('src', message.spinner);
-            statusImg.classList.add('animate__animated', 'animate__fadeInUp');
+            statusImg.classList.add('animated', 'fadeInUp');
             statusMessage.appendChild(statusImg);
 
             let textMessage = document.createElement('div');
@@ -68,17 +179,11 @@ const forms = (state) => {
             statusMessage.appendChild(textMessage);
 
             const formData = new FormData(item);
-            if (item.classList.contains('calc_form')) {
-                for (let key in state) {
-                    formData.append(key, state[key]);
-                }
-            }
-            console.log(formData);
             let api;
             item.closest('.popup-design') || item.classList.contains('calc_form') ? api = path.designer : api = path.question;
             console.log(api);
-            const json = JSON.stringify(Object.fromEntries(formData.entries()));
-            postData(api, json)
+
+            postData(api, formData)
                 .then(res => {
                     console.log(res);
                     statusImg.setAttribute('src', message.ok);
@@ -89,28 +194,16 @@ const forms = (state) => {
                     textMessage.textContent = message.failure;
                 })
                 .finally(() => {
-                    if (item.classList.contains('calc_form')) {
-                        document.querySelector('#size').value = '';
-                        document.querySelector('#material').value = '';
-                        document.querySelector('#options').value = '0';
-                        document.querySelector('.promocode').value = '';
-                    }
                     clearInputs();
-
                     setTimeout(() => {
                         statusMessage.remove();
                         item.style.display = 'block';
-                        item.classList.remove('animate__fadeOutUp');
-                        item.classList.add('animate__fadeInUp');
+                        item.classList.remove('fadeOutUp');
+                        item.classList.add('fadeInUp');
                     }, 5000);
                 });
-
         });
-        
     });
-
-
 };
-
 
 export default forms;
--- src/js/services/requests.js
+++ src/js/services/requests.js
@@ -1,28 +1,50 @@
-const postData = async (url, data) => {
+// const postData = async (url, data) => {
 
-        let res = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: data
-        });
+//         let res = await fetch(url, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-type': 'application/json'
+//             },
+//             body: data
+//         });
 
-        return await res.json();
+//         return await res.json();
         
-};
+// };
     
+
+// const getResource = async (url) => {
+
+//     let res = await fetch(url);
+    
+//     if (!res.ok) {
+//         throw new Error(`Could not fetch ${url}, status ${res.status}`);
+//     }
+
+//     return await res.json();
+        
+// };
+
+// export { postData, getResource };
+
+
+const postData = async (url, data) => {
+    let res = await fetch(url, {
+        method: "POST",
+        body: data
+    });
+
+    return await res.text();
+};
 
 const getResource = async (url) => {
-
     let res = await fetch(url);
-    
+
     if (!res.ok) {
-        throw new Error(`Could not fetch ${url}, status ${res.status}`);
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
 
     return await res.json();
-        
 };
 
-export { postData, getResource };
+export {postData, getResource};
